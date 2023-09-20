b923751630de0d061c77c740e5e8b98a7ab0583c
g <gurwindersingh00064@gmail.com>
2023-09-20 13:17:00 +0530

    m

---
+++ src/app/components/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>
+        I work with Solidity, Hardhat, Ether.js, and Next.js to develop DApps
+        and smart contracts. As an experienced crypto trader since 2020, my
+        proficiency in trading psychology and risk management complements my
+        technical skills, driving my interest in the DeFi and blockchain space
+      </p>
+    </section>
+  );
+};
+
+export default About;
